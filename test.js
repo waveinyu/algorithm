de7ff56d5ccbee4080230bbfde1a61d562827748
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-06-14 09:33:55 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,32 @@
+let arr = [1, 2];
+let arr2 = [3, 4];
+
+console.log(arr);
+console.log(arr2);
+console.log(arr + arr2);
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let newArr = [];
+
+// while (arr.length > 5) {
+//   let moveNum = arr.splice(2, 1)[0];
+//   newArr.push(moveNum);
+// }
+// console.log(newArr);
+
+// --------------------------------------------------------------------
+
+// let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+// let newArr = [];
+
+// function shuffle(arr) {
+//   let answer = 0;
+
+//   while (arr.length > 5) {
+//     let moveNum = arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
+//     newArr.push(moveNum);
+//     answer = newArr;
+//     return answer;
+//   }
+// }
+// console.log(shuffle(arr));
